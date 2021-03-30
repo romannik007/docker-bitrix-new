6da1af11ec7612f62b85697b7f19b6ca57413ea5
roman <romannik007@mail.ru>
2021-03-30 20:05:37 +0300

    correct  build.sh

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ docker create --name bitrix-new --cap-add SYS_ADMIN --security-opt seccomp:uncon
               -v /sys/fs/cgroup:/sys/fs/cgroup:ro  bitrix:new
 docker start bitrix-new
 #docker exec -ti bitrix-new sh -c "yum -y install https://repo.percona.com/yum/percona-release-latest.noarch.rpm;percona-release setup ps57;exit"
-docker exec -ti bitrix-new sh -c "/root/bitrix-env.sh -p -H  bitrix -M $mysql_root_pass;/root/mysql.sh $mysql_root_pass;/root/menu.sh"
+docker exec -ti bitrix-new sh -c "/root/bitrix-env.sh -p -H  bitrix -M $mysql_root_pass;/root/mysql.sh $mysql_root_pass;/root/menu.sh;bash"
 
 #docker exec -ti bitrix-new sh -c "/root/mysql.sh $mysql_root_pass;"
 #docker exec -ti bitrix-new sh -c "yum -y install bx-push-server;exit;"
