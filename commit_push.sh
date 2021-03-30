6707aba898b0e4f258b56ca711971b3f274f4242
r.krilov <>
2021-03-30 22:20:17 +0300

    исправление

--- commit_push.sh
+++ commit_push.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
-
 chmod +x ./entry-web.sh
 chmod +x ./entry-mysql.sh
+#chmod +x ./entry-push.sh
 docker start bitrix-new
 docker cp ./entry-web.sh bitrix-new:/root/
 docker cp ./entry-mysql.sh bitrix-new:/root/
-docker exec -d bitrix-new mkdir /opt/push-server/logs
-docker exec -d bitrix-new yum -y clean all
+docker cp ./bitrix/nginx-config/rtc-server.conf bitrix-new:/etc/nginx/bx/site_enabled/rtc-server.conf
+echo rtc-server.conf
+docker cp ./bitrix/nginx-config/rtc-im_settings.conf bitrix-new:/etc/nginx/bx/settings/rtc-im_settings.conf
+echo rtc-im_settings.conf
+docker cp ./bitrix/nginx-config/rtc-im_subscrider.conf bitrix-new:/etc/nginx/bx/conf/rtc-im_subscrider.conf
+echo rtc-im_subscrider.conf
+docker cp ./bitrix/push-config/push-server-multi bitrix-new:/etc/sysconfig/push-server-multi
+echo push-server-multi
+docker exec bitrix-new sh -c "/etc/init.d/push-server-multi reset;exit"
+docker exec bitrix-new sh -c "systemctl enable push-server;exit"
+docker exec bitrix-new sh -c "systemctl disable --now mysqld;exit"
+docker exec bitrix-new sh -c "systemctl disable --now httpd;exit"
+docker exec bitrix-new sh -c "systemctl disable --now httpd-scale;exit"
+docker exec bitrix-new sh -c "mkdir /opt/push-server/logs;exit"
+docker exec bitrix-new sh -c "yum -y clean all;exit"
 docker stop bitrix-new
 docker commit bitrix-new romannik/bitrix:bitrix-base-new
 docker push romannik/bitrix:bitrix-base-new
