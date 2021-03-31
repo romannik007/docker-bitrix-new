f888118851be98ec2e07b80d826e57b078af5f7a
r.krilov <r.krilov@informunity.ru>
2021-03-31 23:04:45 +0300

    Запуск тестовой ветки push без systemd

--- entry-push.sh
+++ entry-push.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-
-systemctl disable --now mysql
-systemctl disable --now httpd
-systemctl disable --now httpd-scale
-systemctl disable --now nginx
+#systemctl disable --now mysql
+#systemctl disable --now httpd
+#systemctl disable --now httpd-scale
+#systemctl disable --now nginx
+/etc/init.d/push-server-multi start
+/usr/bin/redis-server /etc/redis.conf --daemonize now
 
