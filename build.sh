b70d4af0f68f59cd17f531f4dbfef24fa2ae85ca
r.krilov <r.krilov@informunity.ru>
2021-04-26 13:58:28 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ mkdir -m 777 -p ./bitrix/local
 mkdir -m 777 -p ./bitrix/backup
 mkdir -m 777 -p ./bitrix/mysql
 mkdir -m 777 -p ./bitrix/www
-docker build -t  bitrix:new .
+docker build -t  bitrix:1 .
 docker stop bitrix-new
 docker rm bitrix-new
 docker create --name bitrix-new --cap-add SYS_ADMIN --security-opt seccomp:unconfined --privileged \
               --hostname=bitrix \
               --tmpfs /tmp --tmpfs /run \
-              -v /sys/fs/cgroup:/sys/fs/cgroup:ro  bitrix:new
+              -v /sys/fs/cgroup:/sys/fs/cgroup:ro  bitrix:1
 docker start bitrix-new
 #docker exec -ti bitrix-new sh -c "yum -y install https://repo.percona.com/yum/percona-release-latest.noarch.rpm;percona-release setup ps57;exit"
 docker exec -ti bitrix-new sh -c "/root/bitrix-env.sh -p -H  bitrix -M $mysql_root_pass;/root/menu.sh;bash"
