8989a015d9cf4c0bccc0d61a91645d463b5730bc
r.krilov <r.krilov@informunity.ru>
2021-05-21 11:39:10 +0300

    Update .dockerignore, .gitignore, and 4 more files...

--- load-backup.sh
+++ load-backup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+#bitrix-base-new-backup > invent-stage
+#bitrix-base-new-mysql-backup > invent-stage-mysql
+
 docker load -i bitrix-base-new-backup
 docker load -i bitrix-base-new-mysql-backup
