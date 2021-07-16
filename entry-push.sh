dd3953c89e61a8b652758fcc4bf3f00267a78c24
Roman <r.krilov@informunity.ru>
2021-07-16 17:54:00 +0300

    Update .settings.php, docker-compose.yml, and entry-push.sh

--- entry-push.sh
+++ entry-push.sh
@@ -2,4 +2,10 @@
 
 
 mkdir -p /opt/push-server/logs
+chmod -R 777 /opt/push-server/logs
+mkdir /var/log/supervisor
+chmod -R 777 /var/log/supervisor
+mkdir /var/log/redis
+chmod -R 777 /var/log/redis
+
 exec /usr/bin/supervisord # -c /etc/supervisor.d/supervisord.conf
